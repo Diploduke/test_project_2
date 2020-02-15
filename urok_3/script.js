552646d66bb4cda74ca4bfa23866e876364887c4
Denis <diplodouque@yandex.ru>
2020-02-15 18:21:56 +0300

    Добавление урока 3

---
+++ urok_3/script.js
@@ -0,0 +1,81 @@
+﻿'use strict'
+
+// Задание 3. =======================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Тема 3. Функции =======================================
+
+let calc = (a,b) => a+b;
+
+console.log(calc(3,4));
+console.log(calc(8,4));
+
+let str = "test"; 
+console.log(str.length); // свойства
+
+console.log(str.toUpperCase()); // методы
+console.log(str.toLowerCase()); // методы
+
+let twelve = "12.2";
+console.log(Math.round(twelve)); // округляет
+  
+let twelve1 = "12.2px";
+console.log(parseInt(twelve1));   // 12 преобразует (в т.ч. в другую систему исчисления)
+console.log(parseFloat(twelve1)); // 12.2
+
+// let num = 20;
+
+// function showFirstMessage(text) { // Имя - лучше глагол с привязкой, (аргумент)
+//     alert(text);                    // действия
+//     // let num = 10;
+//     console.log(num);
+// }
+// showFirstMessage("Привет!");
+// console.log(num);
+
+/* *** function declaration - создаются до начала кода, можно
+        вызывать до объявления. *** */
+/*
+console.log(calc(3,4));
+console.log(calc(8,4));
+
+function calc(a,b) {
+     return (a + b);
+}
+*/
+function retVar() {
+    let num = 50;
+    return num; // выводит переменную за пределы функции, закрывает функцию.
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
+
+/* *** function expression - создается, когда код до нее доходит. *** */ 
+
+/*
+let calc = function(a,b) {
+    return (a + b);
+}
+
+console.log(calc(3,4));
+console.log(calc(8,4));
+*/
+
+/* *** Стрелочные - Современный краткий формат (можно не везде). *** */
+
+// Конец темы 3 =======================================
+
+
